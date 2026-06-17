0948bd5f6fdb0bef56ae3c05e506a27d8c05d6f7
marukyss <northern.warship@gmail.com>
2026-06-17 15:59:43 +0200

    get rid of unnecessary comments that were used during writing of code

--- ConwaysGameOfLife/Program.cs
+++ ConwaysGameOfLife/Program.cs
@@ -5,21 +5,11 @@ namespace ConwaysGameOfLife
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // Enables modern visual styles and smooth grid rendering styles
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            // If your compiler complains about the line below, you can uncomment it 
-            // (Required for some modern .NET high-DPI scaling setups)
-            // ApplicationConfiguration.Initialize();
-
-            // Boots up your pure-code MainForm layout
             Application.Run(new MainForm());
         }
     }
